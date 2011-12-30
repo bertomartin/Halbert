cdd2e627a7bb2860f75648635a465ef9c83783aa
Steven G. Harms <steven.harms@gmail.com>
2011-12-30 01:34:23 -0800

    Yes, ship with a default gemset; ensures binaries are avail

--- public/javascripts/custom/tree.coffee
+++ public/javascripts/custom/tree.coffee
@@ -1,4 +1,5 @@
 class Tree
   constructor: (@species) ->
   joke: -> console.log "I make like a #{@constructor.name} and leave!"
+  fall: -> console.log "In autumn the #{@species} is affected with melancholy."
 
--- public/javascripts/custom/tree.js
+++ public/javascripts/custom/tree.js
@@ -6,5 +6,8 @@ Tree = (function() {
   Tree.prototype.joke = function() {
     return console.log("I make like a " + this.constructor.name + " and leave!");
   };
+  Tree.prototype.fall = function() {
+    return console.log("In autumn the " + this.species + " is affected with melancholy.");
+  };
   return Tree;
 })();
