ee92fedeb14a3ca9f3ac8a7229f32bd12615fb7e
Steven G. Harms <steven.harms@gmail.com>
2012-01-22 12:35:50 -0800

    Added first unit test

---
+++ test-js/spec/TunesSpec.coffee
@@ -0,0 +1,36 @@
+albumData = [
+  {
+    title: "Album A"
+    artist: "Artist A"
+    tracks: [
+      {
+        title: "Track A"
+        url: '/music/Album A Track A.mp3'
+      },
+      {
+        title: "Track B"
+        url: '/music/Album A Track B.mp3'
+      }
+    ]
+  },
+  {
+    title: "Album B"
+    artist: "Artist B"
+    tracks: [
+      {
+        title: "Track A"
+        url: '/music/Album B Track A.mp3'
+      },
+      {
+        title: "Track B"
+        url: '/music/Album B Track B.mp3'
+      }
+    ]
+  }
+]
+
+describe 'Album', ->
+  beforeEach ->
+    @album = new Album(albumData[0])
+  it 'creates from data', ->
+    expect(@album.get('tracks').length).toEqual(2)
---
+++ test-js/spec/TunesSpec.js
@@ -0,0 +1,36 @@
+var albumData;
+albumData = [
+  {
+    title: "Album A",
+    artist: "Artist A",
+    tracks: [
+      {
+        title: "Track A",
+        url: '/music/Album A Track A.mp3'
+      }, {
+        title: "Track B",
+        url: '/music/Album A Track B.mp3'
+      }
+    ]
+  }, {
+    title: "Album B",
+    artist: "Artist B",
+    tracks: [
+      {
+        title: "Track A",
+        url: '/music/Album B Track A.mp3'
+      }, {
+        title: "Track B",
+        url: '/music/Album B Track B.mp3'
+      }
+    ]
+  }
+];
+describe('Album', function() {
+  beforeEach(function() {
+    return this.album = new Album(albumData[0]);
+  });
+  return it('creates from data', function() {
+    return expect(this.album.get('tracks').length).toEqual(2);
+  });
+});
