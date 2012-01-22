99e289b74d20dc2b3f239cba512a3a9d36ab9287
Steven G. Harms <steven.harms@gmail.com>
2012-01-22 09:43:03 -0800

    Can create model and view from console

--- public/javascripts/custom/tunes.coffee
+++ public/javascripts/custom/tunes.coffee
@@ -1,2 +1,10 @@
 $ ->
   window.Album = Backbone.Model.extend()
+  window.AlbumView = Backbone.View.extend
+    initialize: ->
+      @template = _.template($('#album-template').html())
+    render: ->
+      renderedContent = @template(@model.toJSON())
+      $(@el).html(renderedContent)
+      @
+
--- public/javascripts/custom/tunes.js
+++ public/javascripts/custom/tunes.js
@@ -1,3 +1,14 @@
 $(function() {
-  return window.Album = Backbone.Model.extend();
+  window.Album = Backbone.Model.extend();
+  return window.AlbumView = Backbone.View.extend({
+    initialize: function() {
+      return this.template = _.template($('#album-template').html());
+    },
+    render: function() {
+      var renderedContent;
+      renderedContent = this.template(this.model.toJSON());
+      $(this.el).html(renderedContent);
+      return this;
+    }
+  });
 });
