290ff2c636b6dc4218301967b020f1e48e9cf221
Steven G. Harms <github@sgharms.oib.com>
2012-09-16 11:13:35 -0700

    Add root.index route

--- public/javascripts/custom/application.js
+++ public/javascripts/custom/application.js
@@ -6,6 +6,9 @@ window.App = Ember.Application.create({
   },
   Router: Ember.Router.extend({
     root:  Ember.Route.extend({
+      index:  Ember.Route.extend({
+        route:  '/'
+      })
     })
   })
 });
