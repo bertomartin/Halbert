369f7ce14c72dd9f552e890642ea63b742dade72
Steven G. Harms <github@sgharms.oib.com>
2012-09-16 10:46:26 -0700

    Add root route per error message

--- public/javascripts/custom/application.js
+++ public/javascripts/custom/application.js
@@ -2,8 +2,9 @@ window.App = Ember.Application.create({
   ready: function(){
     console.log("Created App namespace");
   },
-
   Router: Ember.Router.extend({
+    root:  Ember.Route.extend({
+    })
   })
 });
 
